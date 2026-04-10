6d2532c03451f5c2a58103f6adf18c1346258527
Mehmet Çağrı Doğan <mehmetcagridogan61@gmail.com>
2026-04-10 12:01:13 +0300

    Update shell.h
    Added function bodies,
    
    Added project variables,

--- include/shell.h
+++ include/shell.h
@@ -1,1 +1,23 @@
+#pragma once
 
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+    char *line;
+    char **args;    
+    int status;
+
+    char lsh_read_line() {
+
+    }
+
+    char lsh_parse_line() {
+
+    } 
+
+    int lsh_execute_line() {
+
+        return 1;
+    }
