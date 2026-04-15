f2b868e8c3ad2cafb6b249c6540a2526605c9756
mehmetcagridogan58 <mehmetcagridogan61@gmail.com>
2026-04-15 13:07:53 +0300

    main

--- include/shell.h
+++ include/shell.h
@@ -6,13 +6,46 @@
 
 
     char *line;
-    char **args;    
+    char **args;
     int status;
 
+    #define LSH_RL_BUFFSIZE 1024
     char *lsh_read_line() {
+        int position = 0;
+        int buffsize = LSH_RL_BUFFSIZE;
+        char *buffer = malloc(sizeof(char), buffsize);
+        int c;
 
+        if(!buffer) {
+            fprintf(stderr, "lsh: allocation error");
+            exit(EXIT_FAILURE);
+        }
+
+        while (1) {
+            c = getchar();
+            // every iteration while loop checks if we hit EOF or a newline
+            if (c == EOF || c == '\n' ) {
+                //if EOF you end the line
+                buffer[position] = '\0';
+                return buffer;
+            }
+            else {
+                buffer[position] = c;   
+            }
+            position++;
+
+            if (position >= buffsize){
+                buffer += LSH_RL_BUFFSIZE;
+                buffer = realloc(buffer, buffsize);
+            if (!buffer) {
+                    fprintf(stderr, "lsh: allocation error");
+                    exit(EXIT_FAILURE);
+            }    
+        }        
     }
-
+}
+    #define LSH_TOKEN_BUFFSIZE 64
+    #define LSH_TOKEN_DELIM " \t\r\n\a"
     char **lsh_parse_line() {
 
     } 
--- src/main.c
+++ src/main.c
@@ -20,5 +20,5 @@ int main(int argc, char **argv[]) {
 
     lsh_loop();
     
-return 0;
+return EXIT_SUCCESS;
 }
